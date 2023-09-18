8a3f63aedbf59e6956c4033240dfc06e8331a8b4
Emmah-oo <emmaochonogor715@gmail.com>
2023-09-18 12:34:01 +0100

    feat: implemented swiper

--- src/Components/TvDetails.jsx
+++ src/Components/TvDetails.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const TvDetails = () => {
-  const { id } = useParams()
-  console.log(id)
-  return (
-    <div>TvDetails</div>
-  )
-}
+  const { id } = useParams();
+  console.log(id);
+  return <div className="pt-[8vh]"></div>;
+};
 
-export default TvDetails
+export default TvDetails;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,16 @@ import Imdb from "../Img/Imdb.svg";
 import MovieCard from "../Components/MovieCard";
 import Pagination from "../Components/Pagination";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import 'swiper/css/effect-fade';
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, EffectFade } from "swiper/modules";
+
 const Home = () => {
   const [homeMovie, setHomeMovie] = useState([]);
   const [trendingMovie, setTrendingMovie] = useState([]);
@@ -36,7 +46,7 @@ const Home = () => {
             },
           }
         );
-        setHomeMovie(response.data.results[random]);
+        setHomeMovie(response.data.results);
         // console.log(homeMovie);
         // console.log(response.data.results);
       } catch (error) {
@@ -72,35 +82,51 @@ const Home = () => {
 
   return (
     <div>
-      <div className="w-100% relative home mb-[4vh]">
-        <img
-          src={`${baseImageUrl}${homeMovie.backdrop_path}`}
-          alt=""
-          className="w-[100%] darkened-image"
-        />
-        <div className="absolute top-[50%] px-[6rem] flex flex-col gap-3 items-start">
-          <h1 className="text-white font-bold text-[2rem]">
-            {homeMovie?.title}
-          </h1>
-          <h1 className="text-[1.5rem] text-white font-medium">
-            {homeMovie?.release_date}
-          </h1>
-          <div className="flex gap-2">
-            <img src={Imdb} alt="" />
-            <h1 className="text-white">{homeMovie.vote_average}</h1>
-          </div>
-          <div className="flex gap-2">
-            {matchingGenres?.map((genre, index) => (
-              <h1 className="text-white" key={index}>
-                {genre + "."}
-              </h1>
-            ))}
-          </div>
-          <h1 className="text-white font-medium text-[1rem] max-w-[50%]">
-            {homeMovie?.overview}
-          </h1>
-        </div>
-      </div>
+      <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, EffectFade]}
+        className="mySwiper"
+      >
+        {homeMovie.map((homeMovie, i) => (
+          <SwiperSlide key={i}>
+            <div className="w-100% relative home mb-[4vh]">
+              <img
+                src={`${baseImageUrl}${homeMovie.backdrop_path}`}
+                alt=""
+                className="w-[100%] darkened-image"
+              />
+              <div className="absolute top-[50%] px-[6rem] flex flex-col gap-3 items-start">
+                <h1 className="text-white font-bold text-[2rem]">
+                  {homeMovie?.title}
+                </h1>
+                <h1 className="text-[1.5rem] text-white font-medium">
+                  {homeMovie?.release_date}
+                </h1>
+                <div className="flex gap-2">
+                  <img src={Imdb} alt="" />
+                  <h1 className="text-white">{homeMovie.vote_average}</h1>
+                </div>
+                <div className="flex gap-2">
+                  {matchingGenres?.map((genre, index) => (
+                    <h1 className="text-white" key={index}>
+                      {genre + "."}
+                    </h1>
+                  ))}
+                </div>
+                <h1 className="text-white font-medium text-[1rem] max-w-[50%]">
+                  {homeMovie?.overview}
+                </h1>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <h1 className="px-[6rem] text-3xl font-semibold">Trending</h1>
       <Pagination />
       <div className="grid grid-cols-4 gap-[4rem] px-[6rem]">
